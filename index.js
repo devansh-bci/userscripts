563ed1fdbd181c8b9f5a661b462c5bc3eab5159a
Devansh Gupta <117819668+devansh-bci@users.noreply.github.com>
2025-02-16 23:50:12 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+import { feature_1 } from "./features/feature_1";
+import { feature_2 } from "./features/feature_2";
+
+(function () {
+  "use strict";
+
+  window.feature_1 = false;
+  window.feature_2 = false;
+
+  // Your code here...
+  feature_1();
+  feature_2();
+
+  //   var link = document.createElement("script");
+  //   addPrefixScript.src = "https://storage.googleapis.com/site_resources/site_resources/CAM_Team/add-campaignType-prefix.js?v13";
+  //   document.getElementsByTagName('head')[0].appendChild(addPrefixScript);
+})();
