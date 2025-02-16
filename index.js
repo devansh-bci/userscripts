46685c308be501c7d554b13f001e93eaac2dcd9d
Devansh Gupta <117819668+devansh-bci@users.noreply.github.com>
2025-02-16 23:58:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 import { feature_1 } from "./features/feature_1";
-import { feature_2 } from "./features/feature_2";
 
-window.feature_1 = true;
-window.feature_2 = false;
+window.enableFeature_1 = true;
 
 
 feature_1()
-feature_2()
