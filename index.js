5b17b09a2d1270c63e878e8da88a24446943ebd6
Devansh Gupta <117819668+devansh-bci@users.noreply.github.com>
2025-02-21 16:46:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 (() => {
   "use strict";
-  console.log("index js loaded..."),
+  console.log("index js loaded 1..."),
     window.enableFeature_1 && console.log("Feature 1 loaded successfully..."),
     window.enableFeature_2 && console.log("Feature 2 loaded successfully..."),
     (window.MyLibrary = {});
